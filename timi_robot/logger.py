ae4fc237073cdd5fd3d75ac37d58492a2dc514e8
lixiaolong <lixiaolong@sensoro.com>
2020-07-15 12:07:04 +0800

    release: 1.2.0

--- timi_robot/logger.py
+++ timi_robot/logger.py
@@ -9,16 +9,22 @@ class SensoroLogger(object):
     def __init__(self, url, phones):
         init_sdk(robot_url=url, mentioned_mobiles=phones)
 
-    def log(self, err=None):
+    def log(self, err=None, common_msg=None):
         msg = "{}".format(traceback.format_exc())
         if err:
             msg = "traceback: {}\n\n error: {}".format(traceback.format_exc(), err)
+
+        if common_msg:
+            msg = common_msg
 
         capture_message(text=msg)
 
-    async def async_log(self, err=None):
+    async def async_log(self, err=None, common_msg=None):
         msg = "{}".format(traceback.format_exc())
         if err:
             msg = "traceback: {}\n\n error: {}".format(traceback.format_exc(), err)
+
+        if common_msg:
+            msg = common_msg
 
         await async_capture_message(text=msg)
